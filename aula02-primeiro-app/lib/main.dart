446c367e918528a820b8faaf6013793f14bf1327
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-04-29 09:23:39 -0300

    Aula02 - 29-02-2024

---
+++ aula02-primeiro-app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:aula02/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+    title: 'Aula 02',
+    debugShowCheckedModeBanner: false,
+  ));
+}
