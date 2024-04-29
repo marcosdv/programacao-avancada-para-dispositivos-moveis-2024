0ddfdb715fa72251c77569be35811b905fdcdd43
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-04-29 09:24:21 -0300

    Aula03 - 07-03-2024

---
+++ aula03-widgets-telas/lib/ui/pages/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:aula03/ui/widgets/campo_texto.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final gasolinaController = TextEditingController();
+  final _etanolController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.calculate_outlined, color: Colors.white, size: 36,),
+              SizedBox(width: 8,),
+              Text('Etanol x Gasolina', style: TextStyle(color: Colors.white),),
+            ],
+          ),
+          backgroundColor: Colors.blueAccent.shade700,
+        ),
+        body: Column(
+          children: [
+            CampoTexto(gasolinaController, 'Gasolina', 'R\$'),
+            CampoTexto(_etanolController, 'Etanol', 'R\$'),
+
+            ElevatedButton(onPressed: calcular, child: Text('Calcular')),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void calcular() {
+    final valorGasolina = double.parse(gasolinaController.text);
+    final valorEtanol = double.parse(_etanolController.text);
+
+    if (valorEtanol <= (valorGasolina * 0.7)) {
+      //Etanol é melhor
+    }
+    else {
+      //Gasolina é melhor
+    }
+  }
+}
