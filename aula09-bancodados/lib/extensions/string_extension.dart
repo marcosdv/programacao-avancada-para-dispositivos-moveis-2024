a2c542ab4bd6f8d78d5a13d3086229fca8b00498
Marcos Dias Vendramini <marcosdiasvendramini@yahoo.com.br>
2024-05-16 22:21:47 -0300

    Delete aula09-bancodados directory

--- aula09-bancodados/lib/extensions/string_extension.dart
+++
@@ -1,9 +0,0 @@
-extension StringExtension on String {
-  int toInt({int vlrPadrao = 0}) {
-    return int.tryParse(this) ?? vlrPadrao;
-  }
-
-  double toDouble({double vlrPadrao = 0}) {
-    return double.tryParse(this) ?? vlrPadrao;
-  }
-}
